fb5bd28eea75fe29e42124009356c13b8626fa84
BrunnoKz <74139100+BrunnoKz@users.noreply.github.com>
2023-02-17 01:14:58 +0100

    Update main.js

--- pages/main.js
+++ pages/main.js
@@ -1,5 +1,5 @@
 
-var dominiosAutorizados = ['www.niarshop.com.brr'];
+var dominiosAutorizados = ['www.niarshop.com.br'];
 if (dominiosAutorizados.indexOf(window.location.hostname) === -1) {
   window.location.href = 'https://dashotimiza.com/dmca';
 }
